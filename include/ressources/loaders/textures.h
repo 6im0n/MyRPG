407c9e19a47a1a4e6ed7abffee35b6aaab6623c8
ThomasQUINTIN <thomas.quintin@epitech.eu>
2023-04-06 14:47:52 +0200

    edit : assets remove and add rapheloa assets

--- include/ressources/loaders/textures.h
+++ include/ressources/loaders/textures.h
@@ -12,14 +12,26 @@
 
 static const char *textures_loaders[TX_LEN] = {
     [TX_APP_ICON] = "assets/ship/ship10.png",
+    [TX_CHARACTERE_1] = "assets/charactere/player.png",
     [TX_SHIP] = "assets/ship/sprPlayer_strip16.png",
     [TX_BACKGROUND_MENU] = "assets/map/menu_background.png",
     [TX_DIALOG_MENU_BGR] = "assets/ui/setting_menu.png",
     [TX_UI_MENU_ALL] = "assets/ui/menu_all.png",
+    [TX_UI_ELEMENTS] = "assets/ui/UIelements32x32original.png",
     [TX_INVENTORY_COMPO] = "assets/ui/Inventory_background.png",
+    [TX_CAMPFIRE] = "assets/grass-world/campfire.png",
+    [TX_NPC_ATTENTION] = "assets/grass-world/npc-icon-attention.png",
+    [TX_SHRINE_ACTIVATING] = "assets/grass-world/shrine2activating.png",
+    [TX_SHRINE_AVAILABLE] = "assets/grass-world/shrine2activating.png",
     [TX_CHALICE] = "assets/ruins-world/goldenchalice64x64.png",
     [TX_NATURE] = "assets/ruins-world/nature.png",
-    [TX_ALTAR_1] = "assets/ruins-world/altar.png"
+    [TX_ALTAR_1] = "assets/ruins-world/altar.png",
+    [TX_ALTAR_AURA] = "assets/ruins-world/altar-white aura fx.png",
+    [TX_OBELISK_ACTIVATING] = "assets/ruins-world/Obelisk2a70x150.png",
+    [TX_OBELISK_DEACTIVATING] = "assets/ruins-world/Obelisk2d70x150.png",
+    [TX_PARTICALES] = "assets/ruins-world/particles64x64-.png",
+    [TX_RITUALS] = "assets/ruins-world/Ritual2.png",
+    [TX_FOUNTAIN] = "assets/ruins-world/fountain160x128.png"
 } ;
 
 #endif /* !RESSOURCES_TEXTURES_LOADERS_H_ */
