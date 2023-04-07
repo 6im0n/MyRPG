bd5907096b4079bfb477fe110902b7f4415bbde2
simon GL <simon.gl3135@orange.fr>
2023-04-07 16:16:19 +0200

    add : make a fuction based on the components to make the animation patern of the player

--- src/components/new/annimation.c
+++ src/components/new/annimation.c
@@ -6,6 +6,7 @@
 */
 
 #include "components/components.h"
+#include "components/player.h"
 #include <stdbool.h>
 
 static void annimation_edit(node_component_t *component)
@@ -42,4 +43,33 @@ sfIntRect annimation, int index, int max)
     component->annimation.max = max;
     component->annimation.rect = annimation;
     annimation_edit(component);
+}
+
+static void player_annimation_edit(app_t *app)
+{
+    sfIntRect rect = app->element->player->character->irect;
+    sfIntRect rect_a = app->element->player->character->annimation.rect;
+
+    rect_a.height *= app->element->player->character->annimation.index;
+    rect_a.top *= app->element->player->character->annimation.index;
+    rect_a.left *= app->element->player->character->annimation.index;
+    rect_a.width *= app->element->player->character->annimation.index;
+    rect.height += rect_a.height;
+    rect.top += rect_a.top;
+    rect.left += rect_a.left;
+    rect.width += rect_a.width;
+    sfRectangleShape_setTextureRect(app->element->player->character->shape,
+    rect);
+    app->element->player->character->annimation.index++;
+}
+
+void new_player_annimation(app_t *app, sfIntRect annimation, int index, int max)
+{
+    if (!app->element->player->character)
+        return;
+    app->element->player->character->clock = sfClock_create();
+    app->element->player->character->annimation.index = index;
+    app->element->player->character->annimation.max = max;
+    app->element->player->character->annimation.rect = annimation;
+    player_annimation_edit(app);
 }
